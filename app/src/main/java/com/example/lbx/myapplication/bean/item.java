fbb9233dc6141e523dc0144b22143ff9261b93c7
liangbinxi <249290401@qq.com>
2017-06-30 00:41:19 +0800

    在考试信息类item中添加toString方法

--- app/src/main/java/com/example/lbx/myapplication/bean/item.java
+++ app/src/main/java/com/example/lbx/myapplication/bean/item.java
@@ -49,4 +49,11 @@ public class item {
     public void setQuestionCount(int questionCount) {
         this.questionCount = questionCount;
     }
+
+    @Override
+    public String toString() {
+        return "考试科目:" + subjectTitle + "\n" +
+                "考试数量:" + questionCount +"\n"+
+                "考试时间:" + limitTime + "分钟";
+    }
 }
