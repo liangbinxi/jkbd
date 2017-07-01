799c5fc50716506a10d844eac9aa1400b8bc4af4
liangbinxi <249290401@qq.com>
2017-07-02 00:44:21 +0800

    考试界面的布局

---
+++ app/src/main/java/com/example/lbx/myapplication/activity/ExamActivity.java
@@ -0,0 +1,19 @@
+package com.example.lbx.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lbx.myapplication.R;
+
+/**
+ * Created by lbx on 2017/7/1.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
