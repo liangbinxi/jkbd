174b334f3cce31ffc2f18d98e37d49484f7f2c01
liangbinxi <249290401@qq.com>
2017-07-02 23:40:41 +0800

    修改数据加载

--- app/src/main/java/com/example/lbx/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/lbx/myapplication/activity/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import com.example.lbx.myapplication.R;
+import com.example.lbx.myapplication.bean.Question;
+import com.example.lbx.myapplication.bean.item;
 import com.example.lbx.myapplication.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void test(View view){
-        OkHttpUtils<EditorInfo> utils=new OkHttpUtils<>(getApplicationContext());
+        OkHttpUtils<item> utils=new OkHttpUtils<>(getApplicationContext());
         String uri="http://101.251.196.90:8080/JztkServer/examInfo";
         utils.url(uri)
-                .targetClass(EditorInfo.class)
-                .execute(new OkHttpUtils.OnCompleteListener<EditorInfo>() {
+                .targetClass(item.class)
+                .execute(new OkHttpUtils.OnCompleteListener<item>() {
                     @Override
-                    public void onSuccess(EditorInfo result) {
+                    public void onSuccess(item result) {
                         Log.e("main","result="+result);
                     }
 
@@ -39,4 +41,6 @@ public class MainActivity extends AppCompatActivity {
     public void exit(View view) {
         finish();
     }
+
+
 }
