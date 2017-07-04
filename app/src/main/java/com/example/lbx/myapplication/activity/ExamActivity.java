91d604789ab01faf2e5f3045f10dbb1addd43c4a
liangbinxi <249290401@qq.com>
2017-07-04 23:01:13 +0800

    保存考生的答案

--- app/src/main/java/com/example/lbx/myapplication/activity/ExamActivity.java
+++ app/src/main/java/com/example/lbx/myapplication/activity/ExamActivity.java
@@ -187,7 +187,14 @@ public class ExamActivity extends AppCompatActivity {
             cb.setChecked(false);
         }
     }
-
+    private void saveUserAnswer(){
+        for (int i = 0; i < cbs.length; i++) {
+            if (cbs[i].isChecked()){
+                biz.getExam().setUserAnswer(String.valueOf(i+1));
+                return;
+            }
+        }
+    }
     private void showData(item mitem) {
         tvExamInfo.setText(mitem.toString());
     }
@@ -204,10 +211,12 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     public void preExam(View view) {
+        saveUserAnswer();
         showExam(biz.preQuestion());
     }
 
     public void nextExam(View view) {
+        saveUserAnswer();
         showExam(biz.nextQuestion());
     }
 
